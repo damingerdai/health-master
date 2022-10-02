8e1d1fc1b4db9ed3f49ca06ecc9a4fb6565ef0ca
Arthur Ming <guobin.ming@pwc.com>
2022-10-02 21:22:26 +0800

    feat: setup logger

---
+++ internal/logger/logger.go
@@ -0,0 +1,64 @@
+package logger
+
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Level int8
+
+type Fields map[string]interface{}
+
+const (
+	LevelTrace Level = iota
+	LevelDebug
+	LevelInfo
+	LevelWarn
+	LevelError
+	LevelFatal
+	LevelPanic
+)
+
+type Logger struct {
+	newLogger *log.Logger
+}
+
+func (l Level) String() string {
+	switch l {
+	case LevelTrace:
+		return "trace"
+	case LevelDebug:
+		return "debug"
+	case LevelInfo:
+		return "info"
+	case LevelWarn:
+		return "warn"
+	case LevelError:
+		return "error"
+	case LevelFatal:
+		return "fatal"
+	case LevelPanic:
+		return "panic"
+	}
+	return ""
+}
+
+func NewLogger(level Level) *log.Logger {
+	logger := log.New()
+
+	logger.SetFormatter(&log.TextFormatter{ForceColors: false, FullTimestamp: true})
+	logger.SetOutput(os.Stdout)
+	log.SetFormatter(&log.TextFormatter{ForceColors: false, FullTimestamp: true})
+	log.SetOutput(os.Stdout)
+
+	logLevel, err := log.ParseLevel(level.String())
+	if err != nil {
+		logLevel = log.InfoLevel
+	}
+	logger.SetLevel(logLevel)
+	log.SetLevel(logLevel)
+
+	return logger
+
+}
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/damingerdai/health-master/internal/logger"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello world")
+	r := gin.Default()
+	r.GET("ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+	log := logger.NewLogger(logger.LevelInfo)
+	log.Info("health master server ...")
+	r.Run()
 }
