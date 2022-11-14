b5cef9ee519547e80f9086ec3a41319701f3de90
Arthur Ming <guobin.ming@pwc.com>
2022-11-14 18:12:37 +0800

    feat: clear all toast instance when router changing

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,12 +1,28 @@
 import { Fonts } from '@/components/fonts';
+import { toastInstance } from '@/components/toast';
 import { theme } from '@/lib/theme';
 import { store } from '@/store';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import '../styles/globals.scss';
+import { Router } from 'next/router';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    function clearToast() {
+      toastInstance.closeAll();
+    }
+    Router.events.on('routeChangeStart', clearToast);
+
+    return () => {
+      Router.events.off('routeChangeStart', clearToast);
+    }
+  }, [])
+
+
   return (
     <Provider store={store}>
       <ChakraProvider theme={theme}>
