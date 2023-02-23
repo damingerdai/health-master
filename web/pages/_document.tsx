98bd9443f3012c4c3dcda4be17c50869ac991fe4
Arthur Ming <mingguobin@live.com>
2023-02-23 18:10:28 +0800

    refactor: setup head element to _document.tsx

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -1,15 +1,19 @@
-import { ColorModeScript } from '@chakra-ui/react';
-import NextDocument, {
-  Head, Html, Main, NextScript,
-} from 'next/document';
-import { ToastContainer } from '../components/toast';
-import { theme } from '../lib/theme';
+import { ColorModeScript } from "@chakra-ui/react";
+import NextDocument, { Head, Html, Main, NextScript } from "next/document";
+import { ToastContainer } from "../components/toast";
+import { theme } from "../lib/theme";
 
 export default class Document extends NextDocument {
   render(): JSX.Element {
     return (
       <Html lang="zh">
-        <Head />
+        <Head>
+          <title>Health Master Web</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="health master web" />
+          <meta name="author" content="damingerdai" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,19 +1,11 @@
 import { Box } from '@chakra-ui/react';
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import { UserBloodPressureForm } from '@/components/user-blood-pressure';
 import { ProtectRoute } from '@/components/protect-route';
 import { Siderbar } from '@/components/sidebar';
 
 const Home: NextPage = () => (
   <Box as="main" pb={8}>
-    <Head>
-      <title>Health Master Web</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="description" content="health master web" />
-      <meta name="author" content="damingerdai" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
     <Siderbar>
       <UserBloodPressureForm />
     </Siderbar>
