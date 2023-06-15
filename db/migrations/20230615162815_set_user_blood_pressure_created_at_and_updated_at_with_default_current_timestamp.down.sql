6d58ab5a6d06da39f183367df85413f61d5bc85f
Arthur Ming <guobin.ming@pwc.com>
2023-06-15 16:29:46 +0800

    feat(db): set user blood pressure table with default created_at and updated_at with default current timestamp value

---
+++ db/migrations/20230615162815_set_user_blood_pressure_created_at_and_updated_at_with_default_current_timestamp.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE user_blood_pressure ALTER COLUMN created_at DROP DEFAULT;
+ALTER TABLE user_blood_pressure ALTER COLUMN updated_at DROP DEFAULT;
---
+++ db/migrations/20230615162815_set_user_blood_pressure_created_at_and_updated_at_with_default_current_timestamp.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE user_blood_pressure ALTER COLUMN created_at SET DEFAULT current_timestamp;
+ALTER TABLE user_blood_pressure ALTER COLUMN updated_at SET DEFAULT current_timestamp;
