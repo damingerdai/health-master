62a88a7ed3e04d40478b88c51582248b5334a2b1
Arthur Ming <guobin.ming@pwc.com>
2023-06-28 15:32:35 +0800

    feat(web): add runtime env vars

--- web/pages/api/login.ts
+++ web/pages/api/login.ts
@@ -5,7 +5,7 @@ import { httpClient } from '@/lib/http-client';
 /**
  * https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#use-the-edge-runtime
  */
-export const runtime = 'edge';
+export const runtime = process.env.RUNTIME === 'cloudflare' ? 'edge' : 'nodejs';
 
 export default async function handler(
   req: NextApiRequest,
