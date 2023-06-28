99032290e40e93bc7783d7979d19fd4a998c28b9
Arthur Ming <guobin.ming@pwc.com>
2023-06-28 10:43:37 +0800

    feat: use swr pagintaion feature

--- web/components/user-blood-pressure.tsx
+++ web/components/user-blood-pressure.tsx
@@ -10,7 +10,7 @@ import {
 } from '@chakra-ui/react';
 import useSWR from 'swr';
 import * as React from 'react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { UserBloodPressures } from '@/type/user-blood-pressure';
 import { request } from '@/lib/request';
 import { AddUserBloodPressureModal } from './add-user-blood-pressure';
@@ -38,12 +38,8 @@ export const UserBloodPressureForm: React.FC = () => {
     data: list,
     isLoading,
     mutate,
-  } = useSWR('api/user_blood_pressures', fetcher);
+  } = useSWR(`api/user_blood_pressures?page=${page}`, fetcher);
   const { data, count } = list ?? { data: [], count: 0 };
-
-  useEffect(() => {
-    mutate();
-  }, [page]);
 
   return (
     <>
