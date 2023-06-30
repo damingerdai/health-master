97a7b8c424dbbc5818168c63f1dc8f6829f1d4bb
Arthur Ming <guobin.ming@pwc.com>
2023-06-30 16:23:49 +0800

    feat(web): update delete user blood pressure confirm modal to keep center placement

--- web/components/delete-user-blood-pressure.tsx
+++ web/components/delete-user-blood-pressure.tsx
@@ -1,30 +1,40 @@
 import {
   Button,
-  Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
 } from '@chakra-ui/react';
 import * as React from 'react';
 
 interface DeleteUserBloodPressureModalProps {
-  isOpen: boolean;
-  onClose: () => void;
-  confirm: () => void;
+  isOpen: boolean
+  onClose: () => void
+  confirm: () => void
 }
 
-export const DeleteUserBloodPressureModal: React.FC<DeleteUserBloodPressureModalProps> = (props) => {
+export const DeleteUserBloodPressureModal: React.FC<
+DeleteUserBloodPressureModalProps
+> = (props) => {
   const { isOpen, onClose, confirm } = props;
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
-      <ModalContent>
+      <ModalContent mt={{ base: '50%', sm: '40%' }}>
         <ModalHeader>确定删除？</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>
-          一旦删除将无法撤销！！！
-        </ModalBody>
+        <ModalBody>一旦删除将无法撤销！！！</ModalBody>
         <ModalFooter>
-          <Button colorScheme="gray" mr={3} onClick={onClose}>取消</Button>
-          <Button variant="ghost" onClick={confirm}>确定</Button>
+          <Button colorScheme="gray" mr={3} onClick={onClose}>
+            取消
+          </Button>
+          <Button variant="ghost" onClick={confirm}>
+            确定
+          </Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
