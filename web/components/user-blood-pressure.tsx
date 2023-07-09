f3cd390881c9b3f9a04b6ac9e1747fe72e987424
Arthur Ming <mingguobin@live.com>
2023-07-09 15:55:49 +0800

    feat(web): cache user blood pressure list number to avoid pagation change

--- web/components/user-blood-pressure.tsx
+++ web/components/user-blood-pressure.tsx
@@ -10,7 +10,7 @@ import {
 } from '@chakra-ui/react';
 import useSWR from 'swr';
 import * as React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { UserBloodPressures } from '@/type/user-blood-pressure';
 import { request } from '@/lib/request';
 import { AddUserBloodPressureModal } from './add-user-blood-pressure';
@@ -20,6 +20,8 @@ import { Pagination } from './pagination';
 export const UserBloodPressureForm: React.FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [page, setPage] = useState<number>(1);
+  const [count, setCount] = useState<number>(0);
+
   const fetcher = async () => {
     const res = await request<{
       code: number;
@@ -39,7 +41,13 @@ export const UserBloodPressureForm: React.FC = () => {
     isLoading,
     mutate,
   } = useSWR(`api/user_blood_pressures?page=${page}`, fetcher);
-  const { data, count } = list ?? { data: [], count: 0 };
+  const { data, count: sum } = list ?? { data: [], count: 0 };
+
+  useEffect(() => {
+    if (isLoading) {
+      setCount(sum);
+    }
+  }, [isLoading, sum]);
 
   return (
     <>
