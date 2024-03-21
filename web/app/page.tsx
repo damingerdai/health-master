2428e653d9381065fc62e2b1cb71e097e3f311dc
Arthur Ming <guobin.ming@pwc.com>
2024-03-21 10:21:35 +0800

    chore: remove dashboard

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,18 +1,11 @@
 'use client';
 
 import { Box } from '@chakra-ui/react';
-import { UserBloodPressureForm } from '@/components/user-blood-pressure';
-import { AuthProvider } from '@/components/auth-provider';
-import { Layout } from '@/components/layout';
 
 export default function Home() {
   return (
-    <Layout>
-      <AuthProvider>
-        <Box p={2} h="100%">
-          <UserBloodPressureForm />
-        </Box>
-      </AuthProvider>
-    </Layout>
+    <Box>
+      Health Master App
+    </Box>
   );
 }
