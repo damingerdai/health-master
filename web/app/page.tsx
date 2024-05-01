3b88ce4271441d02eec3f357bd0593a0979e23be
Arthur Ming <guobin.ming@pwc.com>
2024-05-01 09:19:16 +0800

    feat(web): root index view rediect to login view

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,3 +1,12 @@
+import { Center, CircularProgress } from '@chakra-ui/react';
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return null;
+  redirect('/login');
+
+  return (
+    <Center h="100vh">
+      <CircularProgress isIndeterminate color="teal.400" />
+    </Center>
+  );
 }
