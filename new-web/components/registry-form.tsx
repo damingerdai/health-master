536a2094087ab452221aa6ffe635b416d939a35b
damingerdai <mingguobin@live.com>
2025-06-03 01:32:40 +0800

    feat(new-web): add registry from

--- new-web/components/registry-form.tsx
+++ new-web/components/registry-form.tsx
@@ -3,17 +3,14 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export function LoginForm({
+export function RegistryForm({
   className,
   ...props
 }: React.ComponentProps<"form">) {
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Login to your account</h1>
-        <p className="text-muted-foreground text-sm text-balance">
-          Enter your username below to login to your account
-        </p>
+        <h1 className="text-2xl font-bold">Wellcome to Heath Master</h1>
       </div>
       <div className="grid gap-6">
         <div className="grid gap-3">
@@ -33,13 +30,13 @@ export function LoginForm({
           <Input id="password" type="password" required />
         </div>
         <Button type="submit" className="w-full">
-          Login
+          Sign up
         </Button>
       </div>
       <div className="text-center text-sm">
-        Don&apos;t have an account?{" "}
-        <a href="sign-up" className="underline underline-offset-4">
-          Sign up
+        Do you have an account?{" "}
+        <a href="sign-in" className="underline underline-offset-4">
+          Sign in
         </a>
       </div>
     </form>
