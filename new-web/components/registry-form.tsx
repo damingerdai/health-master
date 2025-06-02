d6ea87e4eddab17cc56efc5a40c301b6a9546192
damingerdai <mingguobin@live.com>
2025-06-03 04:28:07 +0800

    feat(new-web): update regisry-from with full the form items

--- new-web/components/registry-form.tsx
+++ new-web/components/registry-form.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 export function RegistryForm({
   className,
@@ -18,17 +19,43 @@ export function RegistryForm({
           <Input id="username" type="text" required />
         </div>
         <div className="grid gap-3">
+          <Label htmlFor="firstName">Frist Name</Label>
+          <Input id="firstName" type="text" required />
+        </div>
+        <div className="grid gap-3">
+          <Label htmlFor="firstName">Last Name</Label>
+          <Input id="lastName" type="text" required />
+        </div>
+        <div className="grid gap-3">
+          <Label htmlFor="gender">Gender</Label>
+          <RadioGroup id="gender" defaultValue="F">
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="M" id="gender-m" />
+              <Label htmlFor="gender-m">Man</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="F" id="gender-f" />
+              <Label htmlFor="gender-f">Felman</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="U" id="gender-u" />
+              <Label htmlFor="gender-u"> Unkown</Label>
+            </div>
+          </RadioGroup>
+        </div>
+        <div className="grid gap-3">
           <div className="flex items-center">
             <Label htmlFor="password">Password</Label>
-            <a
-              href="#"
-              className="ml-auto text-sm underline-offset-4 hover:underline"
-            >
-              Forgot your password?
-            </a>
           </div>
           <Input id="password" type="password" required />
         </div>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="confirmPassword">Confirm Password</Label>
+          </div>
+          <Input id="confirmPassword" type="password" required />
+        </div>
+
         <Button type="submit" className="w-full">
           Sign up
         </Button>
